534e1fe9aa3267c8003de82e2ca76cb88e001629
Junwon Jang <junwonjang2018@u.northwestern.edu>
2019-02-25 23:17:13 -0600

    uses set color to draw

--- crate/src/doodle/mod.rs
+++ crate/src/doodle/mod.rs
@@ -26,37 +26,25 @@ pub fn start_doodle(ws_address: &str) -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
 
-    let closure = Closure::wrap(Box::new(move |event: web_sys::InputEvent| {
-        log(&"color picker closure");
-        log(&event
-            .target().unwrap()
-            .dyn_ref::<web_sys::HtmlInputElement>().unwrap()
-            .value());
-    }) as Box<dyn FnMut(_)>);
-    document
-        .get_element_by_id("color-picker")
-        .expect("document should have .color-picker on DOM")
-        .dyn_ref::<web_sys::HtmlElement>()
-        .expect(".color-picker should be an HtmlElement")
-        .add_event_listener_with_callback("input", closure.as_ref().unchecked_ref())?;
-    closure.forget();
-
-    // color
-    context.set_stroke_style(&JsValue::from_str("rgb(255,0,0,0.5)"));
-    // thickness
-    context.set_line_width(10.0);
-    handle_context_events(context, canvas, ws_address)
+    handle_context_events(document, context, canvas, ws_address)
 }
 
-fn handle_context_events(context: web_sys::CanvasRenderingContext2d, canvas: web_sys::HtmlCanvasElement, ws_address: &str) -> Result<(), JsValue> {
+fn handle_context_events(document: web_sys::Document, context: web_sys::CanvasRenderingContext2d, canvas: web_sys::HtmlCanvasElement, ws_address: &str) -> Result<(), JsValue> {
 
     let m = messaging::Messenger::new(ws_address);
     let context = Rc::new(context);
     let pressed = Rc::new(Cell::new(false));
+    context.set_stroke_style(&JsValue::from_str("#ff0000"));
+
     {
         let context = context.clone();
         let pressed = pressed.clone();
+
         let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            // color
+            context.set_global_alpha(0.6);
+            context.set_line_width(10.0);
+            
             context.begin_path();
             context.move_to(event.offset_x() as f64, event.offset_y() as f64);
             pressed.set(true);
@@ -64,9 +52,11 @@ fn handle_context_events(context: web_sys::CanvasRenderingContext2d, canvas: web
         canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
         closure.forget();
     }
+    
     {
         let context = context.clone();
         let pressed = pressed.clone();
+
         let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
             if pressed.get() {
                 m.sendMessage("sending message on mouse move and pressed");
@@ -76,18 +66,42 @@ fn handle_context_events(context: web_sys::CanvasRenderingContext2d, canvas: web
                 context.move_to(event.offset_x() as f64, event.offset_y() as f64);
             }
         }) as Box<dyn FnMut(_)>);
+
         canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
         closure.forget();
     }
+
     {
         let context = context.clone();
         let pressed = pressed.clone();
+
         let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
             pressed.set(false);
             context.line_to(event.offset_x() as f64, event.offset_y() as f64);
             context.stroke();
         }) as Box<dyn FnMut(_)>);
+
         canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+
+        let closure = Closure::wrap(Box::new(move |event: web_sys::InputEvent| {
+            let color = &event
+                .target().unwrap()
+                .dyn_ref::<web_sys::HtmlInputElement>().unwrap()
+                .value();
+            // log(color);
+            context.set_stroke_style(&JsValue::from_str(color));
+        }) as Box<dyn FnMut(_)>);
+
+        document
+            .get_element_by_id("color-picker")
+            .expect("document should have .color-picker on DOM")
+            .dyn_ref::<web_sys::HtmlElement>()
+            .expect(".color-picker should be an HtmlElement")
+            .add_event_listener_with_callback("input", closure.as_ref().unchecked_ref())?;
         closure.forget();
     }
     Ok(())
