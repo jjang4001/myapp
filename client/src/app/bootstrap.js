e725517da9b2a7be894b0be6d020c3dcfbc0fe17
Junwon Jang <junwonjang2018@u.northwestern.edu>
2019-03-29 21:15:22 -0500

    reduces number of server deployments to 1

--- client/src/app/bootstrap.js
+++ client/src/app/bootstrap.js
@@ -2,7 +2,8 @@
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
 
+import '../style.scss';
+
 import('./index')
   .catch(e => console.error('Error importing crate:', e));
 
-import '../style.scss';
--- client/src/config.ts
+++ client/src/config.ts
@@ -8,7 +8,8 @@ interface EnvConfig {
 
 const config: Config = {
   development: {
-    doodleSocket: 'wss://192.168.99.100/api/doodle'
+    // doodleSocket: 'wss://192.168.99.100/api/doodle'
+    doodleSocket: 'ws://localhost:5000/doodle'
   },
   production: {
     doodleSocket: 'wss://junwonjang.dev/api/doodle'
